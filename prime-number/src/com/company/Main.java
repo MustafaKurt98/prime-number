3a01f05856815cc1651411f8056c81e86a391927
MustafaKurt98 <mustafakurt98@outlook.com>
2021-03-01 15:58:08 +0300

    changeint
    changeint

--- prime-number/src/com/company/Main.java
+++ prime-number/src/com/company/Main.java
@@ -8,19 +8,19 @@ public class Main {
         int a =0;
         System.out.println("enter a number");
         Scanner input=new Scanner(System.in);
-        int sayi=input.nextInt();
-        if (sayi==1){
+        int num=input.nextInt();
+        if (num==1){
             System.out.println("prime numbers are greater than 2");
         }else{
-            for (int i=2; i<=sayi/2;i++){
-                if (sayi%i==0) {
+            for (int i=2; i<=num/2;i++){
+                if (num%i==0) {
                     a++;
                 }
             }
             if (a==0)
-                System.out.println(sayi+" is a prime number");
+                System.out.println(num+" is a prime number");
             else
-                System.out.println(sayi+" is not a prime number");
+                System.out.println(num+" is not a prime number");
         }
     }
 }
